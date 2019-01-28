fda29860967df1a26737f4a0bb03f0bc8c6864a8
David Eiwen <david-e-winlive@live.at>
2019-01-28 20:42:39 +0100

    add initial project files

---
+++ ObjectPath.Builders/WindowsPath.fs
@@ -0,0 +1,13 @@
+﻿namespace ObjectPath.Builders
+
+module WindowsPathBuilder =
+    type public PathSegment =
+        | DriveLetter of DriveLetter : char
+        | RelativeRootPathSegment of Segment : string
+        | PathSegment of PathSegment * Segment : string
+        with
+            member public this.Append newSegment = PathSegment(this, newSegment)
+        end
+
+    let public (/) (beforeSegment : PathSegment) newSegment = beforeSegment.Append newSegment
+    let public (./) driveLetter newSegment = (DriveLetter driveLetter)/newSegment
